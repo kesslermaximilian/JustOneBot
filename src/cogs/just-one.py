3435ef885f72acf4e612e2238c22f6339dd38f1b
Maximilian Keßler <max@maximilian-kessler.de>
2021-05-23 16:28:42 +0200

    first (reasanably) usable version of the bot, added help message

--- src/cogs/just-one.py
+++ src/cogs/just-one.py
@@ -33,14 +33,15 @@ class Game:
     def __init__(self, channel: discord.TextChannel, guesser: discord.Member, bot, wordtype='default'):
         self.channel = channel
         self.guesser = guesser
+        self.guess = ""
         self.word = ""
         self.hints: List[Hint] = []
         self.wordtype = wordtype
+
         self.role: discord.Role = None
         self.sent_messages = []
         self.phase = Phase.initialised
         self.won = None
-        self.word_message = None
         self.bot = bot
         print(f'Game started in channel {self.channel} by user {self.guesser}')
 
@@ -71,13 +72,20 @@ class Game:
 
         self.phase = Phase.show_hints
         await self.add_guesser_to_channel()
+        print('Added user back to channel')
         await self.show_hints()
 
-        guess = await self.wait_for_reaction_from_user(self.guesser)
+        try:
+            guess = await self.wait_for_reaction_from_user(self.guesser)
+        except TimeoutError:
+            print('User has not entered a valid guess')
+            return
+
         if guess is None:
             print('No guess found, aborting')
         self.sent_messages.append(guess)
 
+        self.guess = guess.content
         self.phase = Phase.evaluation  # Start evaluation phase
         self.won = guess.content == self.word  # TODO: have better comparing function
         await self.show_summary()
@@ -132,13 +140,40 @@ class Game:
 
         for hint in self.hints:
             if hint.is_valid():
-                embed.add_field(name=hint.hint_message, value=f'({compute_proper_nickname(hint.author)})')
+                embed.add_field(name=hint.hint_message, value=f'_{compute_proper_nickname(hint.author)}_')
 
         await self.send_message(embed, reaction=False)
 
     async def show_summary(self):
-        # TODO: produce proper summary of the round
-        await self.channel.send(f' You have won the game: {self.won}')
+        s_color = ut.green if self.won else ut.red
+        embed = discord.Embed(
+            title = 'Gewonnen!' if self.won else "Verloren",
+            description = f"Das Wort war: `{self.word}`\n {compute_proper_nickname(self.guesser)} hat `{self.guess}` geraten.",
+            color = s_color,
+        )
+
+        for hint in self.hints:
+            if hint.is_valid():
+                embed.add_field(name=f'`{hint.hint_message}`', value=f'_{compute_proper_nickname(hint.author)}_')
+            else:
+                embed.add_field(name=f"~~`{hint.hint_message}`~~", value=f'_{compute_proper_nickname(hint.author)}_')
+
+        if not self.won:
+            embed.set_footer(text='Nutzt !correct um die Antwort als richtig zu markieren')
+
+        await self.channel.send(embed=embed)
+
+    async def abort(self, reason: str):  # Aborting the current round. Can be either called explicitly or by timeout
+        await self.channel.send(
+            embed=ut.make_embed(
+                title="Runde abgebrochen",
+                value="Die Runde wurde abgebrochen",
+                footer=reason,
+                color=ut.red
+            )
+        )
+        await self.add_guesser_to_channel()
+        await self.clear()
 
     async def clear(self):  # Used to clear chat after round has finished
         # TODO: remove these messages from sent_messages, so we can call the method multiple times
@@ -147,6 +182,9 @@ class Game:
                 await message.delete()
             except discord.NotFound:
                 print(f' The message with content {message.content} could not be deleted')
+        # TODO: Is this the right thing to do?
+        global games
+        games.remove(self)
 
     # Helper methods to manage communication via messages and their reactions
     async def send_message(self, embed, reaction=True, emoji=CHECK_EMOJI) -> discord.Message:
@@ -158,11 +196,8 @@ class Game:
 
     async def wait_for_reaction_to_message(self, message: discord.Message, emoji=CHECK_EMOJI, timeout=60.0) -> bool:
         def check(reaction, user):
-            print(f'Checking reaction {reaction} of user {user} on message {message.content}')
-            print(f'The bot is {self.bot}')
-
             #  Only respond to reactions from non-bots with the correct emoji
-            return not user.bot and str(reaction.emoji) == emoji  # TODO: filter correct message?
+            return not user.bot and str(reaction.emoji) == emoji and reaction.message == message
 
         print(f'waiting for reaction to message {message.content}')
         try:
@@ -195,6 +230,7 @@ class Game:
     async def add_guesser_to_channel(self):
         await self.guesser.remove_roles(self.role)
         await self.role.delete()
+        print('Role deleted')
 
     # External methods called by listeners
     def add_hint(self, message):
@@ -224,10 +260,9 @@ class JustOne(commands.Cog):
         for game in games:
             if game.channel.id == text_channel.id and not game.phase == Phase.finished:
                 print('There is already a game running in this channel, aborting...')
-                # TODO: maybe print a proper error message in the text channel as well?
                 game.sent_messages.append(ctx.message)  # Delete the command at the end of the game
-                await game.send_message(embed=  # Show an error message that a game is running
-                    ut.make_embed(
+                await game.send_message(  # Show an error message that a game is running
+                    embed=ut.make_embed(
                         title="Oops!",
                         value="In diesem Kanal läuft bereits eine Runde JustOne, du kannst keine neue starten",
                         color=ut.red
@@ -239,6 +274,18 @@ class JustOne(commands.Cog):
             game = Game(text_channel, guesser, bot=self.bot)
             games.append(game)
             await game.play()
+
+    @commands.command(name='help_message')
+    async def help_message(self, ctx):
+        await help_message(ctx.channel, ctx.message.author)
+
+    @commands.command(name='abort', help='Bricht die aktuelle Runde im Kanal ab')
+    async def channel(self,ctx : commands.Context):
+        game = find_game(ctx.channel)
+        if game is None:
+            return
+        else:
+            await game.abort('manueller Abbruch')
 
     @commands.command(name='show_answers')
     async def show_answers(self, ctx: commands.Context):
@@ -268,15 +315,6 @@ class JustOne(commands.Cog):
         global tips
         await display_valid_tips(text_channel, tips)
     """
-
-    @commands.command()
-    async def react(self, ctx : commands.Context):
-        game = find_game(ctx.channel)
-        if game is not None:
-            print(game.word_message.reactions)
-            message = await ctx.channel.fetch_message(game.word_message.id)
-            reactions = message.reactions
-            print(reactions)
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -318,11 +356,65 @@ def compute_proper_nickname(member: discord.Member):
     return member.nick if member.nick else member.name
 
 
+async def help_message(channel: discord.TextChannel, member: discord.Member) -> discord.Embed:  # Prints a proper help message for JustOne
+    embed = discord.Embed(
+        title=f'Was is JustOne?',
+        description=f'Hello, {member.mention}. JustOne ist ein beliebtes Paryspiel von  *Ludovic Roudy* und *Bruno Sautter*\n'
+                    f'Das Spiel ist kollaborativ, Ziel ist es, dass eine Person ein ihr unbekanntes Wort errät\n'
+                    f'Dazu wird dieses Wort allen Mitspielern genannt, die sich einen Tipp - *ein* Wort - ausdenken '
+                    f'dürfen. Doch Vorsicht! Geben 2 oder mehr Spieler den (semantisch) gleichen Tipp, so darf der '
+                    f'ratende Spieler diesen nicht ansehen! Ihr dürft euch beim Ausdenken der Tipps also nicht '
+                    f'untereinander absprechen. Seid also geschickt, um so eurem Ahnungslosen Freund zu helfen, das '
+                    f'Lösungswort zu erraten',
+        color=ut.orange,
+        footer='Dieser Bot ist noch unstabil. Bei Bugs, gebt uns auf [GitHub]'
+               '(https://github.com/kesslermaximilian/JustOneBot) Bescheid!)'
+    )
+    embed.add_field(
+        name='Spielstart',
+        value='Startet das Spiel in einem beliebigen Textkanal auf dem Server mit `!play`. '
+              'Derjenige von euch, der den Befehl eingibt, ist auch derjenige, der Raten muss',
+        inline=False
+    )
+    embed.add_field(
+        name='Tippphase',
+        value='Der ratende Spieler kann nun die Nachrichten des Textkanals nicht mehr lesen, macht euch also um'
+              ' Schummler keine Sorgen! Ihr könnt nun alle *einen* Tipp abgeben, indem ihr einfach eine Nachricht'
+              ' in den Kanal schickt. Der Bot löscht diese automatisch, damit ihr sie nicht gegenseitig seht.'
+              ' Doch keine Sorge, der Bot merkt sich natürlich eure Tipps!',
+        inline=False
+    )
+    embed.add_field(
+        name='Fertig? Dann Tipps vergleichen!',
+        value=f'Bestätigt nun dem Bot, dass ihr eure Tipps gegeben habt, indem ihr auf den {CHECK_EMOJI} klickt. '
+              f'Der Bot zeigt euch nun die abgegebenen Antworten an, markiert alle doppelten, indem ihr mit '
+              f'{DISMISS_EMOJI} reagiert. Bestätigt auch dann die Auswahl unter der letzten Nachricht mit einem'
+              f' {CHECK_EMOJI}',
+        inline=False
+    )
+    embed.add_field(
+        name='Raten!',
+        value='Die ratende Person kann nun den Channel automatisch wieder betreten und eine Antwort eingeben, der Bot'
+              ' wertet diese automatisch aus und zeigt euch dann eine Zusammenfassung eurer Runde',
+        inline=False
+    )
+    embed.add_field(
+        name='Viel Spaß!',
+        value='Waruf wartet ihr nowh! Sucht euch einen Kanal und beginnt eure Erste Runde *JustOne*',
+        inline=False
+    )
+
+    await channel.send(embed=embed)
+
 # Method to draw a word used for the game
+
+
 def getword(wordtype):
     return 'Gandhi'
 
 # Setup the bot if this extension is loaded
+
+
 def setup(bot):
     bot.add_cog(JustOne(bot))
 
@@ -330,7 +422,6 @@ def setup(bot):
 """
 Todo:
 get_word : wie lese ich files ein etc
-fetch_reactions: wie auf reaktionen agieren?
 speichern / loggen von games
 listener on_message channel spezifische an/ ausschalten.
 help / settings
