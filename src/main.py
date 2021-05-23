9048b03f4fb9b784fbde6fd727bf4903bb97b742
nonchris <git@chris-ge.de>
2021-05-24 00:52:58 +0200

    Register settings module

--- src/main.py
+++ src/main.py
@@ -39,7 +39,8 @@ bot.remove_command('help')  # unload default help message
 initial_extensions = [
     'cogs.misc',
     'cogs.help',
-    'cogs.just-one'
+    'cogs.just-one',
+    'cogs.settings'
 ]
 
 if __name__ == '__main__':
