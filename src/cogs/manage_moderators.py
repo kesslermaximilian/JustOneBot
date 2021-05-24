0aa316bc8881fab9f3602ab94a06a0847180bf2b
nonchris <git@chris-ge.de>
2021-05-24 17:48:15 +0200

    Add cog for managing (bot internal) moderator roles
    This cog has only one command so far.
    It allows to toggle access (bot) moderator permissions
    -> send the command once and the role has permissions, send it again and the role loses them

---
+++ src/cogs/manage_moderators.py
@@ -0,0 +1,84 @@
+from discord.ext import commands
+import discord
+
+import utils as ut
+import database.db as db
+import database.db_access as dba
+from environment import PREFIX
+from cogs.settings import is_arg, is_arg_int
+
+
+toggle_mod_usage = f"Usage: `{PREFIX}mrole [role id | @role]`"
+
+
+class Access(commands.Cog):
+    """Manage who can configure the bot on your server"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.has_permissions(administrator=True)
+    @commands.command(name="mod-role", aliases=["mrole","config-role", "selrole", "toggle_role", "crole"],
+                      help="Allow roles to select the default wordpools.\n\n"
+                           "This command _toggles_ the permissions for a role. \n"
+                           f"Use the same command to remove it\n\n"
+                           f"{toggle_mod_usage}\n\n"
+                           
+                           f"_administrator permissions required_")
+    async def toggle_mod(self, ctx: commands.Context, *role):
+        if not is_arg(role, 1):
+            await ctx.send(
+                embed=ut.make_embed(
+                    name=f"This command needs one additional argument:\n\n",
+                    value=f"{toggle_mod_usage}\n\n",
+                    color=ut.yellow
+                ))
+            return
+
+        # get role by extracting id and tying to get role on guild
+        id_input = ut.extract_id_from_message(role[0])
+
+        role: discord.Role = ctx.guild.get_role(id_input)
+        if not role:
+            await ctx.send(
+                embed=ut.make_embed(
+                    name="No ID given",
+                    value=f"This command needs a role ID or a role mention as argument to work.\n\n"
+                          f"{toggle_mod_usage}\n\n",
+                    color=ut.yellow
+                )
+            )
+
+        # try to get entry like this from database
+        session = db.open_session()
+        entry = dba.get_setting(ctx.guild.id, str(id_input), setting='mod-role', session=session)
+        # wiping entry if exists to delete privileges
+        if entry:
+            session.delete(entry)
+            session.commit()
+            await ctx.send(
+                embed=ut.make_embed(
+                    name='Removed privileges',
+                    value=f"Successfully removed mod privileges for {role.mention}",
+                    color=ut.orange
+                )
+            )
+            return
+
+        # we need to add a user if we reach this point - let's go
+        dba.add_setting(ctx.guild.id, str(id_input), setting='mod-role', set_by=ctx.author.id)
+        await ctx.send(
+            embed=ut.make_embed(
+                name='Added privileges',
+                value=f"Successfully added mod privileges for {role.mention}\n"
+                      f"Everyone with this role is now able to configure the preset wordpools the bot chooses from.\n",
+                footer="Use the same command again to remove those privileges again.",
+                color=ut.green
+            )
+        )
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Access(bot))
+
+
