0a9433d01ce163025360e7fedbe1f3dc959f8f4f
Maximilian Keßler <max@maximilian-kessler.de>
2021-05-27 00:10:20 +0200

    Set environment variables to default, adjust default timeout to 10 minutes

--- src/environment.py
+++ src/environment.py
@@ -23,16 +23,16 @@ logger = logging.getLogger('my-bot')
 TOKEN = os.getenv("TOKEN")  # reading in the token from config.py file
 
 # loading optional env variables
-PREFIX = load_env("PREFIX", "~")
-VERSION = load_env("VERSION", "alpha 1.0")  # version of the bot
-OWNER_NAME = load_env("OWNER_NAME", "Roman, Maximilian, Chris")   # owner name with tag e.g. pi#3141
-OWNER_ID = int(load_env("OWNER_ID", "296307987981664277"))  # discord id of the owner
+PREFIX = load_env("PREFIX", "jo!")
+VERSION = load_env("VERSION", "unknown")  # version of the bot
+OWNER_NAME = load_env("OWNER_NAME", "unknown")   # owner name with tag e.g. pi#3141
+OWNER_ID = int(load_env("OWNER_ID", "100000000000000000"))  # discord id of the owner
 CHECK_EMOJI = '\u2705'
 DISMISS_EMOJI = '\u274C'
 SKIP_EMOJI = '\u23ed'
 PLAY_AGAIN_CLOSED_EMOJI = '\U0001f501'
 PLAY_AGAIN_OPEN_EMOJI = '\u21a9'
-DEFAULT_TIMEOUT = 300
+DEFAULT_TIMEOUT = 600
 ROLE_NAME = 'JustOne-Guesser'
 DEFAULT_DISTRIBUTION = [('classic_main', 1)]
 
